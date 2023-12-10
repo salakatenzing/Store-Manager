80f6c032b5dacf946d460872e45e48cf6a989315
Tenzing Salaka <salakatenzing@gmail.com>
2023-12-09 21:56:27 -0600

    added cors

--- Store Manager/Program.cs
+++ Store Manager/Program.cs
@@ -6,16 +6,31 @@ using Store_Manager.Seeds;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+//connection to the db
 builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 //put all the repo registrations in ServiceRegistration for cleaner look
 ServiceRegistration.RegisterServices(builder.Services);
 
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCorsPolicy", builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:8080")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+app.UseCors("MyCorsPolicy");
 app.MapControllers();
 
 //seeder
 // Initialize database context
+/*
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -25,6 +40,7 @@ using (var scope = app.Services.CreateScope())
 
     productSeeder.SeedProducts();
 }
+*/
 
 
 
